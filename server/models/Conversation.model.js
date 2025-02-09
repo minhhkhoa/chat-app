4c0807bea2e35dead84a87c5b30c8562c51f33ab
Khoa <khoanguyen.1881@gmail.com>
2025-02-09 21:24:46 +0700

    Create Conversation.model.js && Message.model

---
+++ server/models/Conversation.model.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose')
+
+const messageSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    default: ""
+  },
+  imageUrl: {
+    type: String,
+    default: ""
+  },
+  videoUrl: {
+    type: String,
+    default: ""
+  },
+  seen: {
+    type: Boolean,
+    default: false
+  },
+  msgByUserId: {
+    type: mongoose.Schema.ObjectId,
+    required: true,
+    ref: 'User'
+  }
+}, {
+  timestamps: true
+})
+
+const conversationSchema = new mongoose.Schema({
+  sender: { //-ng gửi
+    type: mongoose.Schema.ObjectId,
+    required: true,
+    ref: 'User' //-Dùng để liên kết (populate) với collection User
+  },
+  receiver: { //- ng nhận
+    type: mongoose.Schema.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+  messages: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Message'
+    }
+  ]
+}, {
+  timestamps: true
+})
+
+const MessageModel = mongoose.model('Message', messageSchema)
+const ConversationModel = mongoose.model('Conversation', conversationSchema)
+
+module.exports = {
+  MessageModel,
+  ConversationModel
+}
