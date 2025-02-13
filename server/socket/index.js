82c1873e1412fc58233c8052278c72b2b034570d
Khoa <khoanguyen.1881@gmail.com>
2025-02-13 18:35:54 +0700

    Update index.js
    fix lỗi lấy lại tin nhắn khi load lại trang

--- server/socket/index.js
+++ server/socket/index.js
@@ -47,7 +47,17 @@ io.on('connection', async (socket) => {
       profile_pic: userDetails?.profile_pic,
       online: onlineUser.has(userId)
     }
-    socket.emit('message-user', payload)
+    socket.emit('message-user', payload);
+
+    //get previous message
+    const getConversationMessage = await ConversationModel.findOne({
+      "$or": [
+        { sender: user?._id, receiver: userId },
+        { sender: userId, receiver: user?._id }
+      ]
+    }).populate('messages').sort({ updatedAt: -1 });
+
+    socket.emit('message', getConversationMessage?.messages || []);
   })
 
   //-new message
